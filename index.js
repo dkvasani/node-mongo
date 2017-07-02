9ae95462a247b6c844888ea85ea1b8e0f60b8176
Dharmesh Vasani <dharmeshkvasani@gmail.com>
2017-07-02 16:19:36 +0530

    Insert and delete command

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ module.exports = {
         MongoClient.connect(url, function (err, db) {
             if (err) throw err;
             console.log("Database created!");
-            return db;
             db.close();
         });
     },
@@ -36,12 +35,46 @@ module.exports = {
 
         MongoClient.connect(url, function (err, db) {
             if (err) throw err;
-            
+
             db.collection(collectionName).insertOne(data, function (err, res) {
                 if (err) throw err;
                 console.log("1 record inserted");
                 db.close();
             });
         });
-    }
+    },
+    insertMultipleDatatTCollection: function (options, collectionName, data) {
+        var mongo = require('mongodb');
+        var MongoClient = require('mongodb').MongoClient;
+        var dbName = options.db_name ? options.db_name : 'mySampleDb'
+        var port = options.port ? options.port : 27017
+        var url = options.url ? options.url : 'mongodb://localhost:' + port + '/' + dbName;
+
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+
+            db.collection(collectionName).insertMany(data, function (err, res) {
+                if (err) throw err;
+                console.log("1 record inserted");
+                db.close();
+            });
+        });
+    },
+    fetchMultipleDatatFromCollection: function (options, collectionName) {
+        var mongo = require('mongodb');
+        var MongoClient = require('mongodb').MongoClient;
+        var dbName = options.db_name ? options.db_name : 'mySampleDb'
+        var port = options.port ? options.port : 27017
+        var url = options.url ? options.url : 'mongodb://localhost:' + port + '/' + dbName;
+
+        MongoClient.connect(url, function (err, db) {
+            if (err) throw err;
+
+            db.collection(collectionName).find({}).toArray(function (err, result) {
+                if (err) throw err;
+                console.log(result);
+                db.close();
+            });
+        });
+    },
 }
--- test.js
+++ test.js
@@ -2,7 +2,14 @@ var mongoDb = require('./index');
 
 var options = {}
 mongoDb.connectMongoDb(options);
-//mongoDb.createCollection(options, 'customer');
+mongoDb.createCollection(options, 'customer');
 
 var data = { name: 'Company Inc', address: 'Highway 37' };
 mongoDb.createCollection(options, 'customer', data)
+var multipleData =  [
+    { name: 'John', address: 'Highway 71'},
+    { name: 'Peter', address: 'Lowstreet 4'},
+    { name: 'Viola', address: 'Sideway 1633'}
+  ];
+mongoDb.insertMultipleDatatTCollection(options, 'customer', multipleData)
+mongoDb.fetchMultipleDatatFromCollection(options, 'customer')
